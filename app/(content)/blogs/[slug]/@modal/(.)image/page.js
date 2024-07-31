87effc4f2f945648144fcf991fdd73b4beb1ce5e
Rohan Bhardwaj <113487993+rohanbhardwaj2005@users.noreply.github.com>
2024-07-31 08:06:22 +0000

    Fixed modal for images

--- app/(content)/blogs/[slug]/@modal/(.)image/page.js
+++ app/(content)/blogs/[slug]/@modal/(.)image/page.js
@@ -15,11 +15,7 @@ export default async function InterceptedImagePage({ params }) {
       <ModalBackDrop />
       <dialog className="modal" open>
         <div className="fullscreen-image">
-          <img
-            src={`/images/news/${blogItem.image}`}
-            style={{ position: "relative" }}
-            alt={blogItem.title}
-          />
+          <img src={`/images/news/${blogItem.image}`} alt={blogItem.title} />
         </div>
       </dialog>
     </>
